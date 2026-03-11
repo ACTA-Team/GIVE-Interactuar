db338f5cd8c2f76ed906d1305af2ef786070ead2
Jesus Borrero <jesufer2018@hotmail.com>
2026-03-11 09:42:21 -0500

    chore: abstract env variables

--- frontend/src/lib/supabase/middleware.ts
+++ frontend/src/lib/supabase/middleware.ts
@@ -2,6 +2,7 @@ import { createServerClient } from '@supabase/ssr';
 import type { NextRequest, NextResponse } from 'next/server';
 import type { CookieToSet } from '@/@types/supabase';
 import type { Database } from './database.types';
+import { supabaseUrl, supabaseKey } from '@/lib/constants/env';
 
 // TODO: wire this up in project/src/middleware.ts to refresh sessions on every request
 // Example:
@@ -16,27 +17,23 @@ export function createMiddlewareClient(
   request: NextRequest,
   response: NextResponse,
 ) {
-  return createServerClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return request.cookies.getAll();
-        },
-        setAll(
-          cookiesToSet: CookieToSet<
-            Parameters<NextResponse['cookies']['set']>[2]
-          >[],
-        ) {
-          cookiesToSet.forEach(({ name, value }) =>
-            request.cookies.set(name, value),
-          );
-          cookiesToSet.forEach(({ name, value, options }) =>
-            response.cookies.set(name, value, options),
-          );
-        },
+  return createServerClient<Database>(supabaseUrl!, supabaseKey!, {
+    cookies: {
+      getAll() {
+        return request.cookies.getAll();
+      },
+      setAll(
+        cookiesToSet: CookieToSet<
+          Parameters<NextResponse['cookies']['set']>[2]
+        >[],
+      ) {
+        cookiesToSet.forEach(({ name, value }) =>
+          request.cookies.set(name, value),
+        );
+        cookiesToSet.forEach(({ name, value, options }) =>
+          response.cookies.set(name, value, options),
+        );
       },
     },
-  );
+  });
 }

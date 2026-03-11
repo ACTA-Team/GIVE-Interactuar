172a82d6f3d704346ed1abbe3a41d25b4871c657
aguilar1x <aaguilar1x@gmail.com>
2026-03-11 13:02:03 -0500

    chore: move supabase.ts types file to /lib/supabase

--- frontend/src/@types/supabase.ts
+++
@@ -1,9 +0,0 @@
-export interface SupabaseLikeClient {
-  from: (table: string) => unknown;
-}
-
-export interface CookieToSet<TOptions = unknown> {
-  name: string;
-  value: string;
-  options?: TOptions;
-}

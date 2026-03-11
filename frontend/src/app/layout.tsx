f8b2f2ec588fed79713a518435f7c985f52194ad
aguilar1x <aaguilar1x@gmail.com>
2026-03-11 17:17:58 -0500

    feat: add fonts manrope

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,13 +5,41 @@ import { Providers } from './providers';
 
 const manrope = localFont({
   src: [
-    { path: './fonts/manrope/Manrope-ExtraLight.otf', weight: '200', style: 'normal' },
-    { path: './fonts/manrope/Manrope-Light.otf',      weight: '300', style: 'normal' },
-    { path: './fonts/manrope/Manrope-Regular.otf',    weight: '400', style: 'normal' },
-    { path: './fonts/manrope/Manrope-Medium.otf',     weight: '500', style: 'normal' },
-    { path: './fonts/manrope/Manrope-SemiBold.otf',   weight: '600', style: 'normal' },
-    { path: './fonts/manrope/Manrope-Bold.otf',       weight: '700', style: 'normal' },
-    { path: './fonts/manrope/Manrope-ExtraBold.otf',  weight: '800', style: 'normal' },
+    {
+      path: './fonts/manrope/Manrope-ExtraLight.otf',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: './fonts/manrope/Manrope-Light.otf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: './fonts/manrope/Manrope-Regular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/manrope/Manrope-Medium.otf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/manrope/Manrope-SemiBold.otf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './fonts/manrope/Manrope-Bold.otf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './fonts/manrope/Manrope-ExtraBold.otf',
+      weight: '800',
+      style: 'normal',
+    },
   ],
   variable: '--font-manrope',
   display: 'swap',

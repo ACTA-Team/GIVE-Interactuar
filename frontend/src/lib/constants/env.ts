20fee429f1f900c4a6aec51f5441a1feecf58550
Jesus Borrero <jesufer2018@hotmail.com>
2026-03-11 12:45:15 -0500

    chore: abstract env variables (#12)

---
+++ frontend/src/lib/constants/env.ts
@@ -0,0 +1,5 @@
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+export { supabaseUrl, supabaseKey, supabaseRoleKey };
